9a7b8389ea4c83d3562aadd03d03984b0d378b4f
yfl <1970115644@qq.com>
2025-12-23 20:33:12 +0800

    feat：新增班级管理CRUD

--- src/main/java/org/example/service/impl/DeptServiceImpl.java
+++ src/main/java/org/example/service/impl/DeptServiceImpl.java
@@ -1,7 +1,46 @@
 package org.example.service.impl;
 
+import org.example.mapper.DeptMapper;
+import org.example.pojo.Dept;
+import org.example.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
-public class DeptServiceImpl {
+public class DeptServiceImpl implements DeptService {
+
+    @Autowired
+    private DeptMapper deptMapper;
+
+    @Override
+    public List<Dept> findAll() {
+        return deptMapper.findAll();
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        deptMapper.deleteById(id);
+    }
+
+    @Override
+    public void insert(Dept dept) {
+        dept.setCreateTime(LocalDateTime.now());
+        dept.setUpdateTime(LocalDateTime.now());
+        deptMapper.insert(dept);
+    }
+
+    @Override
+    public Dept getById(Integer id) {
+        Dept dept = deptMapper.getInfo(id);
+        return dept;
+    }
+
+    @Override
+    public void update(Dept dept) {
+        dept.setUpdateTime(LocalDateTime.now());
+        deptMapper.update(dept);
+    }
 }
